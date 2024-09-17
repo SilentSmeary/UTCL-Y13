1afae9b92b15ee4f012950737358da09bac5fb1a
James <barowikajames@gmail.com>
2024-09-17 10:04:06 +0100

    added: start of php

---
+++ Part 6 - PHP Learning/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo '<b>First bit of code</b>';
+    // Comment One
+    echo '<p class="first-heading" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni natus in, id libero vero quas! Rerum asperiores blanditiis quos nam dolores quasi, aut, dolorem modi sed doloremque praesentium. Quae, voluptate!</p>';
+    /* Comment Two */
+    echo '<p class="first-heading" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni natus in, id libero vero quas! Rerum asperiores blanditiis quos nam dolores quasi, aut, dolorem modi sed doloremque praesentium. Quae, voluptate!</p>';
+    echo '<p class="first-heading" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni natus in, id libero vero quas! Rerum asperiores blanditiis quos nam dolores quasi, aut, dolorem modi sed doloremque praesentium. Quae, voluptate!</p>';
+
+    
+    ?>
+</body>
+</html>
