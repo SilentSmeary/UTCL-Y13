2256acdd4459f113417ee92b468e5268aac85be2
James <barowikajames@gmail.com>
2024-09-18 09:40:52 +0100

    added: php operators

---
+++ Part 7 - PHP Operators/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=x, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $cookie = 23;
+    $cost = 6;
+    $first_name = 'Steven';
+
+    $_1 = 1;
+    $_2 = 2;
+    $_3 = 3;
+    $_4 = 4;
+    $_5 = 5;
+    $_6 = 6;
+    $_7 = 7;
+    $_8 = 8; 
+
+    // Addition
+    echo $_1 + $_2;
+    echo '<br>';
+    // Subtraction
+    echo $_1 - $_2;
+    echo '<br>';
+    // Multiplication
+    echo $_1 * $_2;
+    echo '<br>';
+    // Divison
+    echo $_1 / $_2;
+    echo '<br>';
+    // Modulus
+    echo $_1 % $_2;
+    echo '<br>';
+    // Exponentation
+    echo $_1 ** $_2;
+    echo '<br>';
+    // Increment
+    echo $_8 ++;
+    echo '<br>';
+    // Increment
+    echo $_8 --;
+    echo '<br>';
+    ?>
+</body>
+</html>
