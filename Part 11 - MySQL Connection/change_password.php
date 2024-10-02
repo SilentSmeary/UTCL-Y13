8e60c402d6e9dfdcf8f1878052f7dc4e2ccc07f7
James <barowikajames@gmail.com>
2024-10-02 22:39:33 +0100

    added: audit-log updates

--- Part 11 - MySQL Connection/change_password.php
+++ Part 11 - MySQL Connection/change_password.php
@@ -18,18 +18,62 @@
     echo "<br>";
 
     if (!password_verify($cpassword, $result['password'])) {
+        $act = "failed_password_update";
+        $source = "change_password.php";
+        $logtime = time();
+
+        $sql = "INSERT INTO audit_log (user_id, activity_type, source, date) VALUES(?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+
+
+        $stmt->bindParam(1, $user_id);
+        $stmt->bindParam(2, $act);
+        $stmt->bindParam(3, $source);
+        $stmt->bindParam(4, $logtime);
+
+        $stmt->execute();
 //        Fail audit entry
         session_destroy();
         header("refresh:5; url=index.html");
     } elseif ($npassword != $cnpassword) {
+        $act = "failed_password_update";
+        $source = "change_password.php";
+        $logtime = time();
+
+        $sql = "INSERT INTO audit_log (user_id, activity_type, source, date) VALUES(?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+
+
+        $stmt->bindParam(1, $user_id);
+        $stmt->bindParam(2, $act);
+        $stmt->bindParam(3, $source);
+        $stmt->bindParam(4, $logtime);
+
+        $stmt->execute();
 //        Failed audit entry
-        header("refresh:5; url=change_password.html");
+        header("refresh:5; url=login.html");
     } else {
         $new_hashed_password = password_hash($npassword, PASSWORD_DEFAULT);
         $sql = "UPDATE mem SET password = ? WHERE userid = ?";
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(1,$new_hashed_password);
         $stmt->bindParam(2,$user_id);
+        $stmt->execute();
+
+
+        $act = "successful_password_update";
+        $source = "change_password.php";
+        $logtime = time();
+
+        $sql = "INSERT INTO audit_log (user_id, activity_type, source, date) VALUES(?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+
+
+        $stmt->bindParam(1, $user_id);
+        $stmt->bindParam(2, $act);
+        $stmt->bindParam(3, $source);
+        $stmt->bindParam(4, $logtime);
+
         $stmt->execute();
         session_destroy();
         header("refresh:5; url=login.html");
